e9f04920ee6efcf99e5f107b17510bffcb76a251
portia1230 <qingfeng1230@gmail.com>
2017-06-25 15:43:51 -0700

    Initial commit
    buggy

---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Portia Wang on 6/22/17.
+//  Copyright © 2017 Portia Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController{
+
+    
+
+    var posts = [Post]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UserService.posts(for: User.current) { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
+    }
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+}
